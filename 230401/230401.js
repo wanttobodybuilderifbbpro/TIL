b853278edf295bebde081d8d57d73578a7db9f52
박재형 <wogud114@gmail.com>
2023-04-01 16:34:03 +0900

    note to practicec answer

---
+++ 230401/230401.js
@@ -0,0 +1,24 @@
+//matter of the type of the Number, Sentences
+let material1 = 3;
+let material2 = '3';
+let material3 = 10;
+let material4 = '4';
+let material5 = 4;
+
+//result of operation
+let result1 = material2 + material4;
+let result2 = Number(material2 + material4);
+
+/* example
+let result1 = material2 + material4;
+let result2 = Number(result1);
+ */
+
+// make the sentence '34' and number 34.
+// please note to the this page.
+
+//test code
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
---
+++ 230401/230401question.md
@@ -0,0 +1,9 @@
+Question Practice 20230401
+
+숫자형을 담고 있는 변수들(material1, material3, material5)과 문자열을 담고 있는 변수들(material2, material4)이 있습니다.
+
+변수들끼리 연산을 하여 result1에는 문자열 '34'를, result2에는 숫자형 34를 만들어 넣어 주세요!
+
+단, 절대 숫자값을 사용하지 마세요. result1과, result2는 반드시 material1~5 변수들의 연산식만 할당해야 합니다.
+
+실습 결과는 다음과 같아야 합니다.
