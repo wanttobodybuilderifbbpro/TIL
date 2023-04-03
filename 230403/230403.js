78da24e41b2cd62cb700c756db92b5d9ddd5bdf1
박재형 <wogud114@gmail.com>
2023-04-03 10:30:36 +0900

    another method

---
+++ 230403/230403.js
@@ -0,0 +1,18 @@
+//Using parameter height, make the function "CheckHeight"
+function checkHeight(hegiht){
+  //note codes to this part.
+  const LIMIT = 140;
+  let passMessage = '탑승이 가능합니다.';
+  let failMessage = '탑승이 불가능합니다.';
+
+  if(LIMIT >= 140){
+    console.log(passMessage);
+  } else{
+    console.log(failMessage);
+  }
+}
+
+//test codes
+checkHeight(140);
+checkHeight(135);
+checkHeight(170);
